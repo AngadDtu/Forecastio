ddac0a4c52847eefb638e40d65595a9fdf5ddf2d
Angad Singh <angad.dtu2012@gmail.com>
2016-08-16 22:36:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/angad/forecastio/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.angad.forecastio;
+
+import android.support.annotation.StringDef;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG =MainActivity.class.getSimpleName() ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String apiKey="a65e6661daf2ae7d51c04026725ebd54";
+        double longitude=37.8267;
+        double latitude=-122.423;
+        String foreCast="https://api.forecast.io/forecast/" + apiKey + "/" + longitude + "," + latitude;
+        OkHttpClient client=new OkHttpClient();
+        Request request=new  Request.Builder()
+                .url(foreCast)
+                .build();
+        Call call=client.newCall(request);
+        try {
+            Response response = call.execute();
+            if(response.isSuccessful()){
+                Log.i(TAG,response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG,"NETWORKING ERRROR");
+        }
+    }
+}
