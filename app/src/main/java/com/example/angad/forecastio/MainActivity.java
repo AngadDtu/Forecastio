ce203f58dfdc8a6fb7a95cb0477593ff1b87cbd0
Angad Singh <angad.dtu2012@gmail.com>
2016-08-17 16:15:04 +0530

    Network Checking Problem Solved

--- app/src/main/java/com/example/angad/forecastio/MainActivity.java
+++ app/src/main/java/com/example/angad/forecastio/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.angad.forecastio;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -23,38 +27,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String apiKey="a65e6661daf2ae7d51c04026725ebd54";
-        double longitude=37.8267;
+        double longitude= 37.8267;
         double latitude=-122.423;
         String foreCast="https://api.forecast.io/forecast/" + apiKey + "/" + longitude + "," + latitude;
-        OkHttpClient client=new OkHttpClient();
-        Request request=new  Request.Builder()
-                .url(foreCast)
-                .build();
-        Call call=client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
+        if(isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(foreCast)
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
 
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-
-                    if(response.isSuccessful()){
-                        Log.v(TAG,response.body().string());
-                    }
-                    else{
-                        Log.v(TAG,response.body().string());
-                        alertUserAboutError();
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG,"NETWORKING ERRROR");
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, response.body().string());
+                        } else {
+                            Log.v(TAG, response.body().string());
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "NETWORKING ERRROR");
+                    }
+                }
+            });
+        }
+        else{
+            Toast.makeText(this, R.string.network_error_toast_message,Toast.LENGTH_LONG).show();
+        }
         Log.d(TAG,"Main UI is working");
 
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo=manager.getActiveNetworkInfo();
+        boolean isActive=false;
+        if(networkInfo!=null && networkInfo.isConnected()){
+            isActive=true;
+        }
+        return  isActive;
     }
 
     private void alertUserAboutError() {
