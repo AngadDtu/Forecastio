580238d12a4f6aaa3305bdf3358f0888fe1bbad9
Angad Singh <angad.dtu2012@gmail.com>
2016-08-17 13:43:14 +0530

    Making our App Aynchronous

--- app/src/main/java/com/example/angad/forecastio/MainActivity.java
+++ app/src/main/java/com/example/angad/forecastio/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -29,13 +30,25 @@ public class MainActivity extends AppCompatActivity {
                 .url(foreCast)
                 .build();
         Call call=client.newCall(request);
-        try {
-            Response response = call.execute();
-            if(response.isSuccessful()){
-                Log.i(TAG,response.body().string());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
             }
-        } catch (IOException e) {
-            Log.e(TAG,"NETWORKING ERRROR");
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+
+                    if(response.isSuccessful()){
+                        Log.i(TAG,response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG,"NETWORKING ERRROR");
+                }
+            }
+        });
+        Log.d(TAG,"Main UI is working");
+
     }
 }
